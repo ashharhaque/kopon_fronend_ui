84d649d64dca9f49467fb1219bf45da7ae229665
ashhar <ashharul.haque@adglobal360.com>
2024-03-22 23:48:37 +0530

    image added

--- my-app/src/app/(views)/layout.js
+++ my-app/src/app/(views)/layout.js
@@ -1,6 +1,7 @@
 "use client";
-import { Hind_Madurai } from "next/font/google";
 import { useState } from "react";
+import Image from "next/image";
+import Logo from "./../../../public/logo.jpg";
 // import { IconCross2 } from "../_components/_icon/_socialMediaIcons/svgicon";
 import { IconCross2, MenuIcon } from "@/icons/_socialMediaIcons/svgicon";
 function layoutViews({ children }) {
@@ -8,32 +9,28 @@ function layoutViews({ children }) {
   return (
     <>
       <div className="flex flex-col bg-purple-600  min-h-dvh justify-between ">
-        <header className="bg-green-700 h-[10dvh]">
-          <div
-            className="float-right pt-2"
-            onClick={() => {
-              console.log("on menu toggle--->");
-              setMenuToggleOpen(!menuToggleOpen);
-            }}
-          >
-            {menuToggleOpen ? (
-              <IconCross2 className={`transition-all delay-150 ${menuToggleOpen?"":""}`} />
-            ) : (
-              <MenuIcon className={`${menuToggleOpen}`} />
-            )}
-            
-              {/* <IconCross2 className={`transition-all delay-150 ease-in-out${menuToggleOpen?"hidden":"visible"}`} />
-           
-              <MenuIcon className={`transition-all delay-150 ease-in-out ${!menuToggleOpen?"hidden":"visible"}`} /> */}
-           
-          </div>
-
+        <header className="flex flex-row justify-end bg-green-700 h-[10dvh]">
+           {/* <div className="h-[10px] aspect-square"> */}
+           <Image
+              src={Logo}
+              priority
+              height={30}
+              width={30}
+              alt="logo image"
+              className="bg-red-600"
+            />
+            {/* </div> */}
+          
+          
           {/* <nav
             className={`flex flex-col py-6 pr-4 clear-both  items-center w-[50%] mx-auto transition-all ease-in-out duration-700 ${menuToggleOpen?"translate-y-8  bg-yellow-400 ":"-translate-y-full invisible "}`}
           > */}
-           <nav
-            className={`flex flex-col py-6 pr-4 clear-both  items-center w-[50%] mx-auto transition-all  ease-in-out duration-700 origin-top ${menuToggleOpen?" bg-yellow-400 ":"scale-y-0 invisible "}`}
+          <nav
+            className={`flex flex-col py-6 pr-4  mt-auto items-center w-[50%] mx-auto transition-all  ease-in-out duration-700 origin-top ${
+              menuToggleOpen ? " bg-yellow-400 translate-y-[30%] " : "scale-y-0 invisible "
+            }`}
           >
+           
             <span
               className={`transition-all ease-in-out delay-75 ${
                 menuToggleOpen
@@ -80,6 +77,28 @@ function layoutViews({ children }) {
               BLOG
             </span>
           </nav>
+          <div
+            className=" pt-2"
+            onClick={() => {
+              console.log("on menu toggle--->");
+              setMenuToggleOpen(!menuToggleOpen);
+            }}
+          >
+           
+            {menuToggleOpen ? (
+              <IconCross2
+                className={`transition-all delay-150 ${
+                  menuToggleOpen ? "" : ""
+                }`}
+              />
+            ) : (
+              <MenuIcon className={`${menuToggleOpen}`} />
+            )}
+
+            {/* <IconCross2 className={`transition-all delay-150 ease-in-out${menuToggleOpen?"hidden":"visible"}`} />
+           
+              <MenuIcon className={`transition-all delay-150 ease-in-out ${!menuToggleOpen?"hidden":"visible"}`} /> */}
+          </div>
         </header>
         <div className="">{children}</div>
         <footer className="bg-blue-700">footer</footer>
