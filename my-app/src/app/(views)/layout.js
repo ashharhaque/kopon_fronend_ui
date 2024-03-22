00e703e057ff8e9ba6073978e0a88c26fef094bc
ashhar <ashharul.haque@adglobal360.com>
2024-03-22 11:32:49 +0530

    icon moved to file

--- my-app/src/app/(views)/layout.js
+++ my-app/src/app/(views)/layout.js
@@ -1,39 +1,72 @@
-"use client"
+"use client";
 import { Hind_Madurai } from "next/font/google";
 import { useState } from "react";
+// import { IconCross2 } from "../_components/_icon/_socialMediaIcons/svgicon";
+import { IconCross2, MenuIcon } from "@/icons/_socialMediaIcons/svgicon";
 function layoutViews({ children }) {
-  const [menuToggleOpen,setMenuToggleOpen]=useState(false);
+  const [menuToggleOpen, setMenuToggleOpen] = useState(false);
   return (
     <>
       <div className="flex flex-col bg-purple-600  min-h-dvh justify-between ">
         <header className="bg-green-700 h-[10dvh]">
-          <div className="float-right pt-2" onClick={()=>{
-            console.log("on menu toggle--->");
-            setMenuToggleOpen(!menuToggleOpen);
-          }}>
-            <svg
-              className="feather feather-menu"
-              fill="none"
-              height="24"
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              viewBox="0 0 24 24"
-              width="24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <line x1="3" x2="21" y1="12" y2="12" />
-              <line x1="3" x2="21" y1="6" y2="6" />
-              <line x1="3" x2="21" y1="18" y2="18" />
-            </svg>
+          <div
+            className="float-right pt-2"
+            onClick={() => {
+              console.log("on menu toggle--->");
+              setMenuToggleOpen(!menuToggleOpen);
+            }}
+          >
+            {menuToggleOpen?<IconCross2 />: <MenuIcon />}    
           </div>
-          <nav className={`flex flex-col py-6 pr-4 clear-both bg-yellow-400 w-[50%] items-center mx-auto`}>
-            <span className={`transition-all ease-in-out delay-75 ${menuToggleOpen?"-translate-y-3 " : "-translate-y-full invisible"}`}>ABOUT US</span>
-            <span className={`transition-all ease-in-out delay-100 ${menuToggleOpen?"-translate-y-3 " : "-translate-y-full invisible"}`}>FEATURES</span>
-            <span className={`transition-all ease-in-out delay-150 ${menuToggleOpen?"-translate-y-3 " : "-translate-y-full invisible"}`}>TESTIMONIALS</span>
-            <span className={`transition-all ease-in-out delay-200 ${menuToggleOpen?"-translate-y-3 " : "-translate-y-full invisible"}`}>HOME</span>
-            <span className={`transition-all ease-in-out delay-300 ${menuToggleOpen?"-translate-y-3 " : "-translate-y-full invisible"}`}>BLOG</span>
+
+          <nav
+            className={`flex flex-col py-6 pr-4 clear-both bg-yellow-400 w-[50%] items-center mx-auto`}
+          >
+            <span
+              className={`transition-all ease-in-out delay-75 ${
+                menuToggleOpen
+                  ? "-translate-y-3 "
+                  : "-translate-y-full invisible"
+              }`}
+            >
+              ABOUT US
+            </span>
+            <span
+              className={`transition-all ease-in-out delay-100 ${
+                menuToggleOpen
+                  ? "-translate-y-3 "
+                  : "-translate-y-full invisible"
+              }`}
+            >
+              FEATURES
+            </span>
+            <span
+              className={`transition-all ease-in-out delay-150 ${
+                menuToggleOpen
+                  ? "-translate-y-3 "
+                  : "-translate-y-full invisible"
+              }`}
+            >
+              TESTIMONIALS
+            </span>
+            <span
+              className={`transition-all ease-in-out delay-200 ${
+                menuToggleOpen
+                  ? "-translate-y-3 "
+                  : "-translate-y-full invisible"
+              }`}
+            >
+              HOME
+            </span>
+            <span
+              className={`transition-all ease-in-out delay-300 ${
+                menuToggleOpen
+                  ? "-translate-y-3 "
+                  : "-translate-y-full invisible"
+              }`}
+            >
+              BLOG
+            </span>
           </nav>
         </header>
         <div className="">{children}</div>
---
+++ my-app/src/app/_components/_icon/_socialMediaIcons/svgicon.js
@@ -0,0 +1,38 @@
+// icon:cross-2 | Radix Icons https://icons.radix-ui.com/ | WorkOS
+import * as React from "react";
+
+function IconCross2(props) {
+  return (
+    <svg fill="none" viewBox="0 0 15 15" height="1em" width="1em" {...props}>
+      <path
+        fill="currentColor"
+        fillRule="evenodd"
+        d="M11.782 4.032a.575.575 0 10-.813-.814L7.5 6.687 4.032 3.218a.575.575 0 00-.814.814L6.687 7.5l-3.469 3.468a.575.575 0 00.814.814L7.5 8.313l3.469 3.469a.575.575 0 00.813-.814L8.313 7.5l3.469-3.468z"
+        clipRule="evenodd"
+      />
+    </svg>
+  );
+}
+
+function MenuIcon(props){
+  return(
+    <svg
+              className="feather feather-menu"
+              fill="none"
+              height="24"
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              viewBox="0 0 24 24"
+              width="24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <line x1="3" x2="21" y1="12" y2="12" />
+              <line x1="3" x2="21" y1="6" y2="6" />
+              <line x1="3" x2="21" y1="18" y2="18" />
+            </svg>
+  )
+}
+
+export {IconCross2,MenuIcon} ;
