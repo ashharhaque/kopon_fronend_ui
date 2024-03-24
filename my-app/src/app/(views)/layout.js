634604d28ce2ae7b952c84bdcef93c8dfaaeccc3
ashhar <ashharul.haque@adglobal360.com>
2024-03-24 14:20:00 +0530

    transparent image is added

--- my-app/src/app/(views)/layout.js
+++ my-app/src/app/(views)/layout.js
@@ -2,7 +2,7 @@
 import { useState } from "react";
 import Image from "next/image";
 import Logo from "./../../../public/logo.jpg";
-import KoponLogo from "./../../../public/178073738_padded_logo.png"
+import KoponLogo from "./../../../public/Kopon_transparent.png"
 // import { IconCross2 } from "../_components/_icon/_socialMediaIcons/svgicon";
 import { IconCross2, MenuIcon } from "@/icons/_socialMediaIcons/svgicon";
 function layoutViews({ children }) {
@@ -10,7 +10,7 @@ function layoutViews({ children }) {
   return (
     <>
       <div className="flex flex-col bg-gradient-to-b from-emerald-50 to-teal-400   min-h-dvh justify-between ">
-        <header className="flex flex-row justify-around bg-green-700 h-[10dvh] relative ">
+        <header className="flex flex-row justify-around h-[10dvh] relative ">
           <Image
             // src={Logo}
             // src={"/logo.jpg"}
@@ -19,7 +19,7 @@ function layoutViews({ children }) {
             // height={50}
             // width={50}
             alt="logo image"
-            className="bg-red-600 self-start h-[90%] w-[35%] object-cover mt-1"
+            className="self-start h-[90%] w-[35%] object-cover mt-1"
           />
 
           {/* <nav
@@ -27,7 +27,7 @@ function layoutViews({ children }) {
           > */}
           {/* <h12 className="bg-red-600  ">QOPON</h12> */}
           <nav
-            className={`flex  flex-col py-6 pr-4  absolute top-full left-1/4 items-center w-[40%]  transition-all  ease-in-out duration-700 origin-top md:bg-fuchsia-700  md:flex md:flex-row md:self-stretch md:justify-around md:p-0 md:relative md:top-0 md:left-0 md:transition-none ${
+            className={`flex  flex-col py-6 pr-4  absolute top-full left-1/4 items-center w-[40%]  transition-all  ease-in-out duration-700 origin-top   md:flex md:flex-row md:self-stretch md:justify-around md:p-0 md:relative md:top-0 md:left-0 md:transition-none ${
               menuToggleOpen
                 ? " bg-yellow-400 translate-y-[0%] md:bg-white "
                 : "scale-y-0 invisible md:visible md:scale-y-100"
