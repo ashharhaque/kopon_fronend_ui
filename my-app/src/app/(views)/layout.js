724c074e99868d858cb01732e28642612068e6a2
ashhar <ashharul.haque@adglobal360.com>
2024-03-24 16:14:11 +0530

    font

--- my-app/src/app/(views)/layout.js
+++ my-app/src/app/(views)/layout.js
@@ -10,7 +10,7 @@ function layoutViews({ children }) {
   return (
     <>
       <div className="flex flex-col bg-gradient-to-b from-emerald-50 to-teal-400   min-h-dvh justify-between ">
-        <header className="flex flex-row justify-around h-[10dvh] relative ">
+        <header className="flex flex-row justify-around h-[10dvh] relative bg-green-500">
           <Image
             // src={Logo}
             // src={"/logo.jpg"}
@@ -19,7 +19,7 @@ function layoutViews({ children }) {
             // height={50}
             // width={50}
             alt="logo image"
-            className="self-start h-[90%] w-[35%] object-cover mt-1"
+            className="bg-red-600 self-start h-[90%] w-[35%]  mt-1 object-cover"
           />
 
           {/* <nav
@@ -27,7 +27,7 @@ function layoutViews({ children }) {
           > */}
           {/* <h12 className="bg-red-600  ">QOPON</h12> */}
           <nav
-            className={`flex  flex-col py-6 pr-4  absolute top-full left-1/4 items-center w-[40%]  transition-all  ease-in-out duration-700 origin-top   md:flex md:flex-row md:self-stretch md:justify-around md:p-0 md:relative md:top-0 md:left-0 md:transition-none ${
+            className={`bg-fuchsia-700 flex  flex-col py-6 pr-4  absolute top-full left-1/4 items-center w-[40%]  transition-all  ease-in-out duration-700 origin-top   md:flex md:flex-row md:self-stretch md:justify-around md:p-0 md:relative md:top-0 md:left-0 md:transition-none ${
               menuToggleOpen
                 ? " bg-zinc-50 translate-y-[0%] md:bg-white "
                 : "scale-y-0 invisible md:visible md:scale-y-100"
@@ -52,7 +52,7 @@ function layoutViews({ children }) {
               FEATURES
             </span>
             <span
-              className={`transition-all ease-in-out delay-150 ${
+              className={` transition-all ease-in-out delay-150 ${
                 menuToggleOpen
                   ? "-translate-y-3 "
                   : "-translate-y-full invisible"
