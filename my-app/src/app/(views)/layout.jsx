b18fd7d3778257512a8b2b7aff50ae8a78d9d15a
ashhar <ashharul.haque@adglobal360.com>
2024-03-25 15:44:56 +0530

    phone number moved

--- my-app/src/app/(views)/layout.jsx
+++ my-app/src/app/(views)/layout.jsx
@@ -133,10 +133,10 @@ function layoutViews({ children }) {
             <div className="bg-teal-200 p-3 text-center">
             <h3>Contact</h3>
               <ul>
-                <li className="bg-yellow-600 relative"><span><AdressIcon className="bg-red-600 inline-block "/>Adress-saket,saidulajayab,westend marg,saket</span></li>
-                <li><span><CallIcon className="bg-pink-400 inline-block"/>Phone Number</span></li>
-                <li><span><EmailIcon className="bg-yellow-500 inline-block"/>Email</span></li>
-                <li><span><FaxMachine className="inline-block"/>Fax</span></li>
+                <li className="bg-yellow-600 relative"><span><AdressIcon className="bg-red-600 inline-block absolute top-0 left-0"/>Adress-saket,saidulajayab,westend marg,saket</span></li>
+                <li className="bg-zinc-200 relative"><span><CallIcon className="bg-pink-400 inline-block absolute top-0 left-0"/>Phone Number</span></li>
+                <li className="bg-orange-400 relative"><span><EmailIcon className="bg-yellow-500 inline-block absolute top-0 left-0"/>Email</span></li>
+                <li className="bg-pink-400 relative"><span><FaxMachine className="inline-block absolute top-0 left-0"/>Fax</span></li>
               </ul>
             </div>
           </div>
