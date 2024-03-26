2441153f0edf74ef6fbbb60c9ed14917aa94350a
ashhar <ashharul.haque@adglobal360.com>
2024-03-26 20:17:23 +0530

    some changes

--- my-app/src/app/_views/_LandingPage/index.jsx
+++ my-app/src/app/_views/_LandingPage/index.jsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
-import CouponImage from "@/public/coupon_image.jpeg";
+import CouponImage from "@/public/coupon_image_transparent.png";
 function LandingPage() {
   return (
     <>
-    <div className="h-[40dvh] w-[100dvw] relative">
-        <Image src={CouponImage} className=" object-cover"/>
+    <div className="bg-yellow-300 h-[40dvh] w-[100%] relative">
+        <Image src={CouponImage} style={{
+            "objectFit":"cover"
+        }} className="bg-red-500"/>
     </div>
     </>
   )
