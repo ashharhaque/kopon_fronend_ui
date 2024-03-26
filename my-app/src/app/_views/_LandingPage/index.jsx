43ce3dc071c1550bab7e69c2a9ceae670feaa162
ashhar <ashharul.haque@adglobal360.com>
2024-03-26 20:09:51 +0530

    image added

--- my-app/src/app/_views/_LandingPage/index.jsx
+++ my-app/src/app/_views/_LandingPage/index.jsx
@@ -1,8 +1,12 @@
-
-
+import Image from "next/image";
+import CouponImage from "@/public/coupon_image.jpeg";
 function LandingPage() {
   return (
-    <div>LandingPage</div>
+    <>
+    <div className="h-[40dvh] w-[100dvw] relative">
+        <Image src={CouponImage} className=" object-cover"/>
+    </div>
+    </>
   )
 }
 
