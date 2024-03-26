244e3b44f0af8fd46824a2ca1d1ff2489a6dd738
ashhar <ashharul.haque@adglobal360.com>
2024-03-26 22:33:22 +0530

    project change

--- my-app/src/app/_views/_LandingPage/index.jsx
+++ my-app/src/app/_views/_LandingPage/index.jsx
@@ -1,26 +1,49 @@
 import Image from "next/image";
 import DelieveryPartnerImage from "@/public/delieveryPartnerTransparent.png";
+import FastFoodImage from "@/public/fastFoodsTransparent.png";
 function LandingPage() {
   return (
     <>
       <div className="bg-yellow-300 md:flex justify-around relative m-10">
         <article className="bg-pink-400 basis-[40%] text-center flex items-center">
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis,
-          accusamus quos qui magni facilis quam ex quod numquam tenetur
-          doloremque quas dignissimos, quasi beatae sunt earum inventore. Unde,
-          similique molestias? Iure quis dolorem mollitia soluta ducimus,
-          distinctio voluptas nobis! Magnam distinctio ex est. Maiores, voluptas
-          velit illum suscipit ab perferendis asperiores eos facilis soluta
-          aliquid accusantium doloribus ullam ut vero? Reprehenderit, recusandae
-          sit</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis,
+            accusamus quos qui magni facilis quam ex quod numquam tenetur
+            doloremque quas dignissimos, quasi beatae sunt earum inventore.
+            Unde, similique molestias? Iure quis dolorem mollitia soluta
+            ducimus, distinctio voluptas nobis! Magnam distinctio ex est.
+            Maiores, voluptas velit illum suscipit ab perferendis asperiores eos
+            facilis soluta aliquid accusantium doloribus ullam ut vero?
+            Reprehenderit, recusandae sit
+          </p>
         </article>
         <div className="relative  ">
-        <Image
-          src={DelieveryPartnerImage}
-          className="bg-red-500 object-contain min-h-[100%] w-[100%] md:h-[30%]"
-        />
+          <Image
+            src={DelieveryPartnerImage}
+            className="bg-red-500 object-contain min-h-[100%] w-[100%] md:h-[30%]"
+          />
         </div>
-        
+      </div>
+      <div className="bg-yellow-300 md:flex justify-around relative m-10">
+      <article className="bg-pink-400 basis-[40%] text-center flex items-center">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis,
+            accusamus quos qui magni facilis quam ex quod numquam tenetur
+            doloremque quas dignissimos, quasi beatae sunt earum inventore.
+            Unde, similique molestias? Iure quis dolorem mollitia soluta
+            ducimus, distinctio voluptas nobis! Magnam distinctio ex est.
+            Maiores, voluptas velit illum suscipit ab perferendis asperiores eos
+            facilis soluta aliquid accusantium doloribus ullam ut vero?
+            Reprehenderit, recusandae sit
+          </p>
+        </article>
+        <div className="relative  ">
+          <Image
+            src={FastFoodImage}
+            className="bg-red-500 object-contain min-h-[100%] w-[100%] md:h-[30%]"
+          />
+        </div>
+       
       </div>
     </>
   );
