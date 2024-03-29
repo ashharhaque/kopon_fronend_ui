a659cd2027213a6e25229dfb1a040d2e6939a6c0
ashhar <ashharul.haque@adglobal360.com>
2024-03-29 14:25:34 +0530

    carousel

--- my-app/src/app/_commonComponents/_ui/carousel.jsx
+++ my-app/src/app/_commonComponents/_ui/carousel.jsx
@@ -1,8 +1,14 @@
 "use client"
-import { useState,useEffect } from "react"
+import { useState,useEffect } from "react";
+import Image from "next/image";
 function Carousel({className,src}) {
   return (
-    <div className={`${className} `}>Carousel</div>
+    <div className={`${className}]`}>
+        <Image
+            src={src[0]}
+            className="w-[100%] bg-yellow-400 object-cover"
+        />
+    </div>
   )
 }
 
