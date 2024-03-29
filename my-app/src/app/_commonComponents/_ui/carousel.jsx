ac0fee8cd86d132e6d22907e52d2b90aba4b0b7e
ashhar <ashharul.haque@adglobal360.com>
2024-03-30 01:08:36 +0530

    change

--- my-app/src/app/_commonComponents/_ui/carousel.jsx
+++ my-app/src/app/_commonComponents/_ui/carousel.jsx
@@ -61,23 +61,38 @@ function Carousel({ className, src, ImageWidth, ImageHeight }) {
 
   //   );
   return (
-    <div className="bg-yellow-500 flex justify-center  w-[50%] h-[100%] relative">
-      <Image
+    <div className="bg-yellow-500 flex justify-center  w-[50%] h-[60dvh] relative">
+      {/* <Image
         src={src[0]}
-        // fill
-        className={`bg-red-500 absolute object-contain translate-x-[0px] opacity-100 scale-100  transition-all ease-in-out delay-300 duration-1000  ${transitionstart==0?"translate-x-0 opacity-100 scale-100":"translate-x-[200%] opacity-0 scale-0"}`}
+        // fill="responsive"
+        className={`bg-red-500 w-[100%]  object-contain translate-x-[0px] opacity-100 scale-100  transition-all ease-in-out delay-300 duration-1000  ${transitionstart==0?"translate-x-0 opacity-100 scale-100":"translate-x-[100%] opacity-0 scale-0"}`}
       />
       <Image
         src={src[1]}
         // fill
-        className={`bg-red-500 absolute object-contain translate-x-[0px] opacity-100 scale-100   transition-all ease-in-out delay-300 duration-1000  ${transitionstart==1?"translate-x-0 opacity-100 scale-100":"translate-x-[200%] opacity-0 scale-0"}`}
+        className={`bg-red-500 w-[100%] object-contain translate-x-[0px] opacity-100 scale-100   transition-all ease-in-out delay-300 duration-1000  ${transitionstart==1?"translate-x-0 opacity-100 scale-100":"translate-x-[100%] opacity-0 scale-0"}`}
       />
       <Image
         src={src[2]}
         // fill
-        className={`bg-red-500  object-contain translate-x-[0px] opacity-100 scale-100   transition-all ease-in-out delay-300 duration-1000  ${transitionstart==2?"translate-x-0 opacity-100 scale-100":"translate-x-[200%] opacity-0 scale-0"}`}
+        className={`bg-red-500 w-[100%]  object-contain translate-x-[0px] opacity-100 scale-100   transition-all ease-in-out delay-300 duration-1000  ${transitionstart==2?"translate-x-0 opacity-100 scale-100":"translate-x-[100%] opacity-0 scale-0"}`}
+      /> */}
+    <div className="bg-red-500 w-[30%] h-[50%] absolute top-1/4">
+    <Image
+        src={src[1]}
+        // fill
+        className={`bg-red-500 w-[100%] object-contain translate-x-[0px] opacity-100 scale-100   transition-all ease-in-out delay-300 duration-1000  ${transitionstart==1?"translate-x-0 opacity-100 scale-100":"translate-x-[100%] opacity-0 scale-0"}`}
       />
-
+    
+    </div>
+    <div className="bg-blue-600 w-[30%] h-[50%] absolute top-1/4">
+    <Image
+        src={src[0]}
+        // fill="responsive"
+        className={`bg-red-500 w-[100%]  object-contain  translate-x-[0px] opacity-100 scale-100  transition-all ease-in-out delay-300 duration-1000  ${transitionstart==0?"translate-x-0 opacity-100 scale-100":"translate-x-[100%] opacity-0 scale-0"}`}
+      />
+    
+    </div>
     </div>
   );
 }
