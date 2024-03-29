47ac9e20b6e4cb0ceba41670e7d120dc8d7b1717
ashhar <ashharul.haque@adglobal360.com>
2024-03-29 14:47:55 +0530

    carousel is implemented

--- my-app/src/app/_commonComponents/_ui/carousel.jsx
+++ my-app/src/app/_commonComponents/_ui/carousel.jsx
@@ -2,10 +2,31 @@
 import { useState,useEffect } from "react";
 import Image from "next/image";
 function Carousel({className,src}) {
+    const [currentSrc,setCurrentSrc]=useState(src[0])
+    useEffect(()=>{
+        let count=0
+        const srcSlider=()=>{
+            try{
+                console.log("inside srcSlider count--->",count);
+                console.log("src length---->",src.length);
+                console.log("src on particular count----->",count%src.length);
+                setCurrentSrc(src[count%src.length])
+                count++;
+            }catch(err){
+                console.log("err in srcSlider---->",err)
+            }
+        }
+        const intervalID = setInterval(srcSlider, 3000);
+        return () => {
+            clearInterval(intervalID);
+          };
+    },[])
   return (
     <div className={`${className}]`}>
         <Image
-            src={src[0]}
+            // src={src[0]}
+            src={currentSrc}
+
             className="w-[100%] bg-yellow-400 object-cover"
         />
     </div>
