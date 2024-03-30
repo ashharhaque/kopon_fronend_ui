850ff75cbc26eb4938ec4ef05060a871c6501fde
ashhar <ashharul.haque@adglobal360.com>
2024-03-31 01:04:15 +0530

    done

--- my-app/src/app/_commonComponents/_ui/carousel.jsx
+++ my-app/src/app/_commonComponents/_ui/carousel.jsx
@@ -111,21 +111,21 @@ function Carousel({ className, src, ImageWidth, ImageHeight }) {
 //     </div>
 //   );
 //0,1,2//total 3 rounds(length of image array)
-//0//transition start=0//add 0% to each and 300% to third child(first child view)
-//1//tranisitionStart=1//add 100% to each and subtract 200% from third child (second child view)
-//2//transitionstart=2//add 100% to remaing 2 and subract -200% from first and //(third child view)
-//3///transitionstart=3//add 100% to each and subtract 200% from second
+//0//transition start=0//add 0% to each and 300% to third child(first child view)//1--0%,2--0%,3-300%
+//1//tranisitionStart=1//add 100% to each and subtract 200% from third child (second child view)//1--100%,2--100%,3--100%
+//2//transitionstart=2//add 100% to remaing 2 and subract -200% from first and //(third child view)//1-- -100%,2--200%,3--200%
+//3///transitionstart=3//add 100% to each and subtract 200% from second//1--200%,2--0%,3--300%
 //repetition start
 //4//transitionTime=4//add 100% to each and subtract 200% from third
 //5//transitionTime=5//add 100% to each and subtract 200% from first
 //6//transitionTime=6//add 100% to each and subtract 200% from second child
 return (
     <div className="bg-violet-500 w-[80%] h-[60vh] flex  overflow-visible">
-      <div className={`bg-green-500 min-w-[100%] h-[100%] transition-all ease-in-out duration-1000 -translate-x-[${transitionstart==0?"00": transitionstart-2==0?((transitionstart-1)*(-1*100)):(transitionstart*100)}%]`}>
+      <div className={`bg-green-500 min-w-[100%] h-[100%] transition-all ease-in-out duration-1000 -translate-x-[${transitionstart==0?"00": transitionstart-2==0?((transitionstart-1)*(-1*100)):transitionstart==src.length?(src.length-1)*(100):(transitionstart*100)}%]`}>
         asfadfaf
       </div>
-      <div className={`bg-yellow-400 min-w-[100%] h-[100%] transition-all ease-in-out duration-1000 -translate-x-[${transitionstart==0?"00": transitionstart-3==0?((transitionstart-1)*(-1*100)):(transitionstart*100)}%]`}>ddddd</div>
-      <div className={`bg-red-500 min-w-[100%] h-[100%]  transition-all ease-in-out duration-1000 -translate-x-[${transitionstart==0?"300": transitionstart-4==0?((transitionstart-1)*(-1*100)):(transitionstart*100)}%]`}>eeee</div>
+      <div className={`bg-yellow-400 min-w-[100%] h-[100%] transition-all ease-in-out duration-1000 -translate-x-[${transitionstart==0?"00": transitionstart-3==0?((transitionstart-1)*(1*100)):(transitionstart*100)}%]`}>ddddd</div>
+      <div className={`bg-red-500 min-w-[100%] h-[100%]  transition-all ease-in-out duration-1000 -translate-x-[${transitionstart==0?"300": transitionstart==1?(src.length-2)*100:transitionstart==src.length?(transitionstart*100):(transitionstart*100)}%]`}>eeee</div>
       {/* <div className={`bg-red-500 min-w-[100%] h-[100%]  transition-all ease-in-out duration-1000 -translate-x-[300)}%]`}>eeee</div> */}
 
     </div>
