730353bea4d7c898d4ea0d7a46e63c1dcc89b0ff
ashhar <ashharul.haque@adglobal360.com>
2024-03-31 01:27:54 +0530

    done

--- my-app/src/app/_commonComponents/_ui/carousel.jsx
+++ my-app/src/app/_commonComponents/_ui/carousel.jsx
@@ -30,104 +30,156 @@ function Carousel({ className, src, ImageWidth, ImageHeight }) {
   //     className={`${className}`}
   //     // className="bg-yellow-500 flex justify-center m-[12px]  w-[100%] h-[60dvh] overflow-hidden  relative"
   //     >
-        // <Image
-        //   src={src[0]}
-        //   fill="responsive"
-        //   //   className={`bg-green-500 object-contain opacity-100 scale-100  transition-all ease-in-out delay-300 duration-1000 `}
-        //   className={`bg-red-500  object-contain translate-x-[0px] opacity-100 scale-100   transition-all ease-in-out delay-300 duration-1000  ${
-        //     transitionstart == 0
-        //       ? "translate-x-0 opacity-100 scale-100"
-        //       : "translate-x-[100%] opacity-0 scale-0"
-        //   }`}
-        // />
+  // <Image
+  //   src={src[0]}
+  //   fill="responsive"
+  //   //   className={`bg-green-500 object-contain opacity-100 scale-100  transition-all ease-in-out delay-300 duration-1000 `}
+  //   className={`bg-red-500  object-contain translate-x-[0px] opacity-100 scale-100   transition-all ease-in-out delay-300 duration-1000  ${
+  //     transitionstart == 0
+  //       ? "translate-x-0 opacity-100 scale-100"
+  //       : "translate-x-[100%] opacity-0 scale-0"
+  //   }`}
+  // />
 
-        // <Image
-        //   src={src[1]}
-        //   fill
-        //   //   className={`  object-contain  opacity-100 scale-100  transition-all ease-in-out delay-300 duration-1000 `}
-        //   className={`bg-red-500  object-contain translate-x-[0px] opacity-100 scale-100   transition-all ease-in-out delay-300 duration-1000  ${
-        //     transitionstart == 1
-        //       ? "translate-x-0 opacity-100 scale-100"
-        //       : "translate-x-[100%] opacity-0 scale-0"
-        //   }`}
-        // />
+  // <Image
+  //   src={src[1]}
+  //   fill
+  //   //   className={`  object-contain  opacity-100 scale-100  transition-all ease-in-out delay-300 duration-1000 `}
+  //   className={`bg-red-500  object-contain translate-x-[0px] opacity-100 scale-100   transition-all ease-in-out delay-300 duration-1000  ${
+  //     transitionstart == 1
+  //       ? "translate-x-0 opacity-100 scale-100"
+  //       : "translate-x-[100%] opacity-0 scale-0"
+  //   }`}
+  // />
 
-        // <Image
-        //   src={src[2]}
-        //   fill
-        //   //   className={`bg-black object-contain  opacity-100 scale-100  transition-all ease-in-out delay-300 duration-1000 `}
-        //   className={`bg-red-500  object-contain translate-x-[0px] opacity-100 scale-100   transition-all ease-in-out delay-300 duration-1000  ${
-        //     transitionstart == 2
-        //       ? "translate-x-0 opacity-100 scale-100"
-        //       : "translate-x-[100%] opacity-0 scale-0"
-        //   }`}
-        // />
+  // <Image
+  //   src={src[2]}
+  //   fill
+  //   //   className={`bg-black object-contain  opacity-100 scale-100  transition-all ease-in-out delay-300 duration-1000 `}
+  //   className={`bg-red-500  object-contain translate-x-[0px] opacity-100 scale-100   transition-all ease-in-out delay-300 duration-1000  ${
+  //     transitionstart == 2
+  //       ? "translate-x-0 opacity-100 scale-100"
+  //       : "translate-x-[100%] opacity-0 scale-0"
+  //   }`}
+  // />
   //     </div>
   //   );
-  console.log("transition start---->",transitionstart)
-//   return (
-//     <div className="bg-violet-500 w-[80%] h-[60vh] flex  overflow-visible">
-//       <div className="bg-green-500 min-w-[100%] h-[100%] -translate-x-[0%]">
-//         asfadfaf
-//       </div>
-//       <div className="bg-yellow-400 min-w-[100%] h-[100%] -translate-x-[0%]">ddddd</div>
-//       <div className="bg-red-500 min-w-[100%] h-[100%] -translate-x-[0%]">eeee</div>
-//     </div>
-//   );
-//0,1,2//total 3 rounds
-//0--starting at 00%
-//1--add 100% to each
-//2--subract -200% from first and add 100% to remaing 2
-//3--(again start the values) and reset to 00%
-// return (
-//     <div className=" w-[80%] h-[60vh] flex  overflow-hidden">
-//       <div className={`relative min-w-[100%] h-[100%] transition-all ease-in-out duration-1000  -translate-x-[${src.length-1==transitionstart?(transitionstart-1)*(-1*100):transitionstart==0?"00":transitionstart*100}%] ${transitionstart==0?"opacity-100":"opacity-0"}`}>
-//      <Image
-//           src={src[0]}
-//           fill="responsive"
-//           //   className={`bg-green-500 object-contain opacity-100 scale-100  transition-all ease-in-out delay-300 duration-1000 `}
-//           className={`  object-contain `}
-//         />
-//       </div>
-//       <div className={`relative min-w-[100%] h-[100%] transition-all ease-in-out duration-1000 -translate-x-[${src.length-1==transitionstart?transitionstart*100:transitionstart==0?"00":transitionstart*100}%] ${transitionstart==1?"opacity-100":"opacity-0"}`}>
-//       <Image
-//           src={src[1]}
-//           fill
-//           //   className={`  object-contain  opacity-100 scale-100  transition-all ease-in-out delay-300 duration-1000 `}
-//           className={` object-contain `}
-          
-//         />
-//       </div>
-//       <div className={`relative min-w-[100%] h-[100%]  transition-all ease-in-out duration-1000 -translate-x-[${src.length-1==transitionstart?transitionstart*100:transitionstart==0?"00":transitionstart*100}%] ${transitionstart==2?"opacity-100":"opacity-0"}`}>
+  console.log("transition start---->", transitionstart);
+  //   return (
+  //     <div className="bg-violet-500 w-[80%] h-[60vh] flex  overflow-visible">
+  //       <div className="bg-green-500 min-w-[100%] h-[100%] -translate-x-[0%]">
+  //         asfadfaf
+  //       </div>
+  //       <div className="bg-yellow-400 min-w-[100%] h-[100%] -translate-x-[0%]">ddddd</div>
+  //       <div className="bg-red-500 min-w-[100%] h-[100%] -translate-x-[0%]">eeee</div>
+  //     </div>
+  //   );
+  //0,1,2//total 3 rounds
+  //0--starting at 00%
+  //1--add 100% to each
+  //2--subract -200% from first and add 100% to remaing 2
+  //3--(again start the values) and reset to 00%
+  // return (
+  //     <div className=" w-[80%] h-[60vh] flex  overflow-hidden">
+  //       <div className={`relative min-w-[100%] h-[100%] transition-all ease-in-out duration-1000  -translate-x-[${src.length-1==transitionstart?(transitionstart-1)*(-1*100):transitionstart==0?"00":transitionstart*100}%] ${transitionstart==0?"opacity-100":"opacity-0"}`}>
+  //      <Image
+  //           src={src[0]}
+  //           fill="responsive"
+  //           //   className={`bg-green-500 object-contain opacity-100 scale-100  transition-all ease-in-out delay-300 duration-1000 `}
+  //           className={`  object-contain `}
+  //         />
+  //       </div>
+  //       <div className={`relative min-w-[100%] h-[100%] transition-all ease-in-out duration-1000 -translate-x-[${src.length-1==transitionstart?transitionstart*100:transitionstart==0?"00":transitionstart*100}%] ${transitionstart==1?"opacity-100":"opacity-0"}`}>
+        // <Image
+        //     src={src[1]}
+        //     fill
+        //     //   className={`  object-contain  opacity-100 scale-100  transition-all ease-in-out delay-300 duration-1000 `}
+        //     className={` object-contain `}
 
-//       <Image
-//           src={src[2]}
-//           fill
-//           //   className={`bg-black object-contain  opacity-100 scale-100  transition-all ease-in-out delay-300 duration-1000 `}
-//           className={`  object-contain `}
-         
-//         />
-//       </div>
-//     </div>
-//   );
-//0,1,2//total 3 rounds(length of image array)
-//0//transition start=0//add 0% to each and 300% to third child(first child view)//1--0%,2--0%,3-300%
-//1//tranisitionStart=1//add 100% to each and subtract 200% from third child (second child view)//1--100%,2--100%,3--100%
-//2//transitionstart=2//add 100% to remaing 2 and subract -200% from first and //(third child view)//1-- -100%,2--200%,3--200%
-//3///transitionstart=3//add 100% to each and subtract 200% from second//1--00%,2--0%,3--300%
-//repetition start
-//4//transitionTime=4//add 100% to each and subtract 200% from third
-//5//transitionTime=5//add 100% to each and subtract 200% from first
-//6//transitionTime=6//add 100% to each and subtract 200% from second child
-return (
-    <div className="bg-violet-500 w-[80%] h-[60vh] flex  overflow-visible">
-      <div className={`bg-green-500 min-w-[100%] h-[100%] transition-all ease-in-out duration-1000 -translate-x-[${transitionstart==0?"00": transitionstart-2==0?((transitionstart-1)*(-1*100)):transitionstart==src.length?(0)*(100):(transitionstart*100)}%] ${transitionstart==0 || transitionstart==3?"opacity-100":"opacity-0"}`}>
-        asfadfaf
+        //   />
+  //       </div>
+  //       <div className={`relative min-w-[100%] h-[100%]  transition-all ease-in-out duration-1000 -translate-x-[${src.length-1==transitionstart?transitionstart*100:transitionstart==0?"00":transitionstart*100}%] ${transitionstart==2?"opacity-100":"opacity-0"}`}>
+
+        // <Image
+        //     src={src[2]}
+        //     fill
+        //     //   className={`bg-black object-contain  opacity-100 scale-100  transition-all ease-in-out delay-300 duration-1000 `}
+        //     className={`  object-contain `}
+
+        //   />
+  //       </div>
+  //     </div>
+  //   );
+  //0,1,2//total 3 rounds(length of image array)
+  //0//transition start=0//add 0% to each and 300% to third child(first child view)//1--0%,2--0%,3-300%
+  //1//tranisitionStart=1//add 100% to each and subtract 200% from third child (second child view)//1--100%,2--100%,3--100%
+  //2//transitionstart=2//add 100% to remaing 2 and subract -200% from first and //(third child view)//1-- -100%,2--200%,3--200%
+  //3///transitionstart=3//add 100% to each and subtract 200% from second//1--00%,2--0%,3--300%
+  //repetition start
+  //4//transitionTime=4//add 100% to each and subtract 200% from third
+  //5//transitionTime=5//add 100% to each and subtract 200% from first
+  //6//transitionTime=6//add 100% to each and subtract 200% from second child
+  return (
+    <div className="bg-violet-500 w-[80%] h-[60vh] flex  overflow-hidden">
+      <div
+        className={`bg-green-500 min-w-[100%] h-[100%] transition-all ease-in-out duration-1000 -translate-x-[${
+          transitionstart == 0
+            ? "00"
+            : transitionstart - 2 == 0
+            ? (transitionstart - 1) * (-1 * 100)
+            : transitionstart == src.length
+            ? 0 * 100
+            : transitionstart * 100
+        }%] ${
+          transitionstart == 0 || transitionstart == 3
+            ? "opacity-100"
+            : "opacity-0"
+        }`}
+      >
+        <Image
+          src={src[0]}
+          fill="responsive"
+          //   className={`bg-green-500 object-contain opacity-100 scale-100  transition-all ease-in-out delay-300 duration-1000 `}
+          className={`  object-contain `}
+        />
       </div>
-      <div className={`bg-yellow-400 min-w-[100%] h-[100%] transition-all ease-in-out duration-1000 -translate-x-[${transitionstart==0?"00":transitionstart==src.length?(0)*(100):(transitionstart*100)}%] ${transitionstart==1?"opacity-100":"opacity-0"}`}>ddddd</div>
-      <div className={`bg-red-500 min-w-[100%] h-[100%]  transition-all ease-in-out duration-1000 -translate-x-[${transitionstart==0?"300": transitionstart==1?(src.length-2)*100:transitionstart==src.length?(transitionstart*100):(transitionstart*100)}%] ${transitionstart==2?"opacity-100":"opacity-0"}`}>eeee</div>
-      {/* <div className={`bg-red-500 min-w-[100%] h-[100%]  transition-all ease-in-out duration-1000 -translate-x-[300)}%]`}>eeee</div> */}
+      <div
+        className={`bg-yellow-400 min-w-[100%] h-[100%] transition-all ease-in-out duration-1000 -translate-x-[${
+          transitionstart == 0
+            ? "00"
+            : transitionstart == src.length
+            ? 0 * 100
+            : transitionstart * 100
+        }%] ${transitionstart == 1 ? "opacity-100" : "opacity-0"}`}
+      >
+         <Image
+            src={src[1]}
+            fill
+            //   className={`  object-contain  opacity-100 scale-100  transition-all ease-in-out delay-300 duration-1000 `}
+            className={` object-contain `}
 
+          />
+      </div>
+      <div
+        className={`bg-red-500 min-w-[100%] h-[100%]  transition-all ease-in-out duration-1000 -translate-x-[${
+          transitionstart == 0
+            ? "300"
+            : transitionstart == 1
+            ? (src.length - 2) * 100
+            : transitionstart == src.length
+            ? transitionstart * 100
+            : transitionstart * 100
+        }%] ${transitionstart == 2 ? "opacity-100" : "opacity-0"}`}
+      >
+         <Image
+            src={src[2]}
+            fill
+            //   className={`bg-black object-contain  opacity-100 scale-100  transition-all ease-in-out delay-300 duration-1000 `}
+            className={`  object-contain `}
+
+          />
+      </div>
+      {/* <div className={`bg-red-500 min-w-[100%] h-[100%]  transition-all ease-in-out duration-1000 -translate-x-[300)}%]`}>eeee</div> */}
     </div>
   );
 }
