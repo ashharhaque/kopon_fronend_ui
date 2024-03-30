accda8d78042fbc6fe36748683eea26a7fea47b5
ashhar <ashharul.haque@adglobal360.com>
2024-03-30 17:45:27 +0530

    carousel making

--- my-app/src/app/_commonComponents/_ui/carousel.jsx
+++ my-app/src/app/_commonComponents/_ui/carousel.jsx
@@ -25,44 +25,53 @@ function Carousel({ className, src, ImageWidth, ImageHeight }) {
     };
   }, []);
 
- 
+  //   return (
+  //     <div
+  //     className={`${className}`}
+  //     // className="bg-yellow-500 flex justify-center m-[12px]  w-[100%] h-[60dvh] overflow-hidden  relative"
+  //     >
+  //       <Image
+  //         src={src[0]}
+  //         fill="responsive"
+  //         //   className={`bg-green-500 object-contain opacity-100 scale-100  transition-all ease-in-out delay-300 duration-1000 `}
+  //         className={`bg-red-500  object-contain translate-x-[0px] opacity-100 scale-100   transition-all ease-in-out delay-300 duration-1000  ${
+  //           transitionstart == 0
+  //             ? "translate-x-0 opacity-100 scale-100"
+  //             : "translate-x-[100%] opacity-0 scale-0"
+  //         }`}
+  //       />
+
+  //       <Image
+  //         src={src[1]}
+  //         fill
+  //         //   className={`  object-contain  opacity-100 scale-100  transition-all ease-in-out delay-300 duration-1000 `}
+  //         className={`bg-red-500  object-contain translate-x-[0px] opacity-100 scale-100   transition-all ease-in-out delay-300 duration-1000  ${
+  //           transitionstart == 1
+  //             ? "translate-x-0 opacity-100 scale-100"
+  //             : "translate-x-[100%] opacity-0 scale-0"
+  //         }`}
+  //       />
+
+  //       <Image
+  //         src={src[2]}
+  //         fill
+  //         //   className={`bg-black object-contain  opacity-100 scale-100  transition-all ease-in-out delay-300 duration-1000 `}
+  //         className={`bg-red-500  object-contain translate-x-[0px] opacity-100 scale-100   transition-all ease-in-out delay-300 duration-1000  ${
+  //           transitionstart == 2
+  //             ? "translate-x-0 opacity-100 scale-100"
+  //             : "translate-x-[100%] opacity-0 scale-0"
+  //         }`}
+  //       />
+  //     </div>
+  //   );
+
   return (
-    <div 
-    className={`${className}`}
-    // className="bg-yellow-500 flex justify-center m-[12px]  w-[100%] h-[60dvh] overflow-hidden  relative"
-    >
-      <Image
-        src={src[0]}
-        fill="responsive"
-        //   className={`bg-green-500 object-contain opacity-100 scale-100  transition-all ease-in-out delay-300 duration-1000 `}
-        className={`bg-red-500  object-contain translate-x-[0px] opacity-100 scale-100   transition-all ease-in-out delay-300 duration-1000  ${
-          transitionstart == 0
-            ? "translate-x-0 opacity-100 scale-100"
-            : "translate-x-[100%] opacity-0 scale-0"
-        }`}
-      />
-
-      <Image
-        src={src[1]}
-        fill
-        //   className={`  object-contain  opacity-100 scale-100  transition-all ease-in-out delay-300 duration-1000 `}
-        className={`bg-red-500  object-contain translate-x-[0px] opacity-100 scale-100   transition-all ease-in-out delay-300 duration-1000  ${
-          transitionstart == 1
-            ? "translate-x-0 opacity-100 scale-100"
-            : "translate-x-[100%] opacity-0 scale-0"
-        }`}
-      />
-
-      <Image
-        src={src[2]}
-        fill
-        //   className={`bg-black object-contain  opacity-100 scale-100  transition-all ease-in-out delay-300 duration-1000 `}
-        className={`bg-red-500  object-contain translate-x-[0px] opacity-100 scale-100   transition-all ease-in-out delay-300 duration-1000  ${
-          transitionstart == 2
-            ? "translate-x-0 opacity-100 scale-100"
-            : "translate-x-[100%] opacity-0 scale-0"
-        }`}
-      />
+    <div className="bg-violet-500 w-[80%] h-[60vh] flex  overflow-visible">
+      <div className="bg-green-500 min-w-[100%] h-[100%] -translate-x-[100%]">
+        asfadfaf
+      </div>
+      <div className="bg-yellow-400 min-w-[100%] h-[100%] -translate-x-[100%]">ddddd</div>
+      <div className="bg-red-500 min-w-[100%] h-[100%] -translate-x-[100%]">eeee</div>
     </div>
   );
 }
