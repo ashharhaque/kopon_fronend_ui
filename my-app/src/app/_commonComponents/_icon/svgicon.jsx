7a14496bd2f2af1b391f6a5d0134af10d4e55d4f
ashhar <ashharul.haque@adglobal360.com>
2024-04-01 20:08:31 +0530

    carousel

--- my-app/src/app/_commonComponents/_icon/svgicon.jsx
+++ my-app/src/app/_commonComponents/_icon/svgicon.jsx
@@ -252,34 +252,35 @@ function InstagramIcon({ className }) {
       </g>
     </svg>
   );
-  function LessThanIcon({className}) {
-    return (
-      <svg
-        className={className}
-        fill="#000000"
-        width="800px"
-        height="800px"
-        viewBox="0 0 32 32"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path d="M 26 6 L 6 15.21875 L 6 16.78125 L 26 26 L 26 23.84375 L 9.46875 16 L 26 8.15625 Z" />
-      </svg>
-    );
-  }
-  function GreaterThanIcon({className}) {
-    return (
-      <svg
-        className={className}
-        fill="#000000"
-        width="800px"
-        height="800px"
-        viewBox="0 0 32 32"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path d="M 6 6 L 6 8.15625 L 22.53125 16 L 6 23.84375 L 6 26 L 26 16.78125 L 26 15.21875 Z" />
-      </svg>
-    );
-  }
+}
+
+function LessThanIcon({className}) {
+  return (
+    <svg
+      className={className}
+      fill="#000000"
+      width="800px"
+      height="800px"
+      viewBox="0 0 32 32"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M 26 6 L 6 15.21875 L 6 16.78125 L 26 26 L 26 23.84375 L 9.46875 16 L 26 8.15625 Z" />
+    </svg>
+  );
+}
+function GreaterThanIcon({className}) {
+  return (
+    <svg
+      className={className}
+      fill="#000000"
+      width="800px"
+      height="800px"
+      viewBox="0 0 32 32"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M 6 6 L 6 8.15625 L 22.53125 16 L 6 23.84375 L 6 26 L 26 16.78125 L 26 15.21875 Z" />
+    </svg>
+  );
 }
 
 export {
