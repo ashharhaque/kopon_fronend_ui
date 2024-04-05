740853661dbe24ef4a107b5e9ca84544b5b353d5
ashhar <ashharul.haque@adglobal360.com>
2024-04-05 14:21:27 +0530

    change

--- my-app/src/app/_views/_LandingPage/index.jsx
+++ my-app/src/app/_views/_LandingPage/index.jsx
@@ -14,46 +14,49 @@ import ContentFixedslide from "@/views/_LandingPage/ContentFixedSlide"
 import Carousel from "@/app/_commonComponents/_ui/carousel";
 function LandingPage() {
   const [isVisible,setIsVisible]=useState(false);
+  const [isVisibleOne,setIsVisibleOne]=useState(false);
+  const [isVisibleTwo,setIsVisibleTwo]=useState(false);
   const observedElementRef=useRef(null);
-  const handleIntersection=async(entries)=>{
-    console.log("inside handleIntersection---->")
+  const observedElementRefTwo=useRef(null);
+  const observedElementRefOne=useRef(null);
+
+  const handleIntersection=async(entries,setVisibility)=>{
+    console.log("inside handleIntersection---->");
     try{
       entries.map((entry,idx)=>{
         console.log("each entry of entries---->",entry.isIntersecting);
-        console.log("intersectio ratio---->",entry)
+        console.log("intersectio ratio---->",entry);
         if(entry.isIntersecting){
-          // entry.target.className="bg-red-500   basis-[40%] text-center flex items-center transition-all duration-1000 translate-x-0 opacity-100"
-         setIsVisible(true)
+          setVisibility(true);
         }else{
-          setIsVisible(false)
+          setVisibility(false);
         }
-        // entry.target.className="bg-red-500   basis-[40%] text-center flex items-center transition-all duration-1000 translate-x-[20px] "
-
       })
     }catch(err){
-      console.log("error in interSection api---->",err)
+      console.log("error in interSection api---->",err);
     }
   }
   useEffect(()=>{
     const observer = new IntersectionObserver(handleIntersection,{
-      // root:null,
-
-      // rootMargin:"-300px",
       threshold:.5
     });
+    const observerOne=new IntersectionObserver((e)=>handleIntersection(e,setIsVisibleOne),{
+      threshold:.5
+    })
+
+    const observerTwo=new IntersectionObserver((e)=>handleIntersection(e,setIsVisibleTwo),{
+      threshold:.5
+    })
    
-      observer.observe(observedElementRef.current)
+      observer.observe(observedElementRef.current);
+      observerTwo.observe(observedElementRefTwo.current);
+      observerOne.observe(observedElementRefOne.current);
     
-    // return observer.disconnect()
-    // return () => {
-    //   if(observedElementRef.current)
-    //   observer.unobserve(observedElementRef.current);
-    // }
   },[])
   return (
     <>
-      <div className=" md:flex justify-around relative m-10">
-        <article className=" basis-[40%] text-center flex items-center">
+      <div className=" md:flex justify-around relative m-10" ref={observedElementRefOne}>
+        <article className={` basis-[40%] text-center flex items-center md:transition-all md:ease-in-out  md:duration-1000 md:translate-x-[-200%] ${isVisibleOne?"bg-red-300 md:translate-x-0 md:opacity-100":"md:translate-x-[-200%] md:opacity-0"} `}>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis,
             accusamus quos qui magni facilis quam ex quod numquam tenetur
@@ -72,8 +75,8 @@ function LandingPage() {
           />
         </div>
       </div>
-      <div className=" md:flex md:flex-row-reverse justify-around relative m-10">
-        <article className=" basis-[40%] text-center flex items-center ">
+      <div className=" md:flex md:flex-row-reverse justify-around relative m-10" ref={observedElementRefTwo}>
+        <article className={` basis-[40%] text-center flex items-center md:transition-all md:ease-in-out  md:duration-1000 md:translate-x-[200%] ${isVisibleTwo?"bg-red-300 md:translate-x-0 md:opacity-100":"md:translate-x-[200%] md:opacity-0"} `}>
           <p className="">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis,
             accusamus quos qui magni facilis quam ex quod numquam tenetur
